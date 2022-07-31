8ffcebd8efa3d9a06e40d27d03c0f23f9de71bbf
Wildan Nugraha <nwildan922@gmail.com>
2022-07-31 21:58:04 +0700

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.json({status:'express runnning.'})
+  res.json({ status: 'express runnning.' })
+})
+
+app.get('/user/:id', (req, res) => {
+  res.json({ status: `user dengan id ${req.params.id}` })
 })
 
 app.listen(port, () => {
